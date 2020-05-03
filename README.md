b6722dd3b498df0206dd40faa4c40f9b08ca3b5d
Cyril PARODI <parodicyril@gmail.com>
2020-05-03 16:14:12 +0200

    st + redshift-gtk startup fix

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Dotfiles for my debian testing configuration.
 
+## st
+Install with `make` and `sudo make clean install`
+
+## Other tools
 Install w/ GNU Stow.
--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Overpass Mono:pixelsize=13:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -86,48 +86,41 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+	/* 8 normal colors */
+	"black",
+	"red3",
+	"green3",
+	"yellow3",
+	"blue2",
+	"magenta3",
+	"cyan3",
+	"gray90",
 
-  /* 8 normal colors */
-  [0] = "#090300", /* black   */
-  [1] = "#db2d20", /* red     */
-  [2] = "#01a252", /* green   */
-  [3] = "#fded02", /* yellow  */
-  [4] = "#01a0e4", /* blue    */
-  [5] = "#a16a94", /* magenta */
-  [6] = "#b5e4f4", /* cyan    */
-  [7] = "#a5a2a2", /* white   */
+	/* 8 bright colors */
+	"gray50",
+	"red",
+	"green",
+	"yellow",
+	"#5c5cff",
+	"magenta",
+	"cyan",
+	"white",
 
-  /* 8 bright colors */
-  [8]  = "#5c5855", /* black   */
-  [9]  = "#db2d20", /* red     */
-  [10] = "#01a252", /* green   */
-  [11] = "#fded02", /* yellow  */
-  [12] = "#01a0e4", /* blue    */
-  [13] = "#a16a94", /* magenta */
-  [14] = "#b5e4f4", /* cyan    */
-  [15] = "#f7f7f7", /* white   */
+	[255] = 0,
 
-  /* special colors */
-  [256] = "#090300", /* background */
-  [257] = "#a5a2a2", /* foreground */
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#cccccc",
+	"#555555",
 };
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor
- */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 257;
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-// unsigned int defaultfg = 15;
-// unsigned int defaultbg = 0;
-// static unsigned int defaultcs = 15;
+unsigned int defaultfg = 7;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
